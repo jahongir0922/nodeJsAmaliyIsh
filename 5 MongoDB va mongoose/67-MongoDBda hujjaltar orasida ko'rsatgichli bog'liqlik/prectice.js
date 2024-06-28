00cb1c508784f5c633f580ee72ab5a61f449b286
jahongir0922 <qodirovjahongir09@gmail.com>
2024-06-28 18:40:43 +0300

    67-darsgacha

---
+++ 5 MongoDB va mongoose/67-MongoDBda hujjaltar orasida ko'rsatgichli bog'liqlik/prectice.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/test")
+  .then(() => {
+    console.log("mongoDBga ulandi");
+  })
+  .catch(() => {
+    console.log("MongoDbga ulanishda xatolik");
+  });
+
+const authorSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+});
+const Author = mongoose.model("Author", authorSchema);
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
+  },
+});
+const Book = mongoose.model("Book", bookSchema);
+
+async function createAuthor(firstName, lastName, emaill) {
+  const author = new Author({
+    lastName: lastName,
+    firstName: firstName,
+    email: email,
+  });
+  await author.save();
+  console.log(author);
+}
+async function createBook(title, authorId) {
+  const book = new Book({
+    title: title,
+    author: authorId,
+  });
+  await book.save();
+  console.log(book);
+}
+async function listBooks() {
+  const books = await Book.find()
+    .populate("author", "firstName -_id")
+    .select("title author");
+  console.log(books);
+}
+// createAuthor("Jahongir", "Qodirov", "email@gmail.com");
+// createBook("2 - e vashshe zo'r kitob", "667ecf80733d43f468f6e128");
+listBooks();
