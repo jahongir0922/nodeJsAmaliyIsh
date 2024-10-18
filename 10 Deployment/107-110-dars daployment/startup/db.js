af3e6f4ee5760913ece4d89b299fb6a5a1c4a755
Jahongir <69594209+jahongir0922@users.noreply.github.com>
2024-10-18 15:46:10 +0500

    18.10.2024

--- 10 Deployment/107-110-dars daployment/startup/db.js
+++ 10 Deployment/107-110-dars daployment/startup/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/test")
+    // .connect("mongodb://localhost/test")
+    .connect(
+      "mongodb+srv://qodirovjahongir09:mongo09@cluster0.rhjof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
     .then(() => {
       console.log("mongoDBga ulandi");
     })
--- 10 Deployment/readme.txt
+++ 10 Deployment/readme.txt
@@ -3,3 +3,11 @@ heroku login
 git init
 git add .
 git commit -m "first commit"
+
+
+
+
+
+<<<vercel>>>
+npm i -g vercel
+vercel
